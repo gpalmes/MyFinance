80e8715d0a06136f8269c2dbfcacd6504b3fd1b1
Guillote <palmesguillermo@gmail.com>
2024-09-27 22:12:34 -0300

    todo

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,16 +1,30 @@
-// App.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './navigation/AppNavigator';
-import { StatusBar } from 'expo-status-bar';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen'; // Ruta correcta
+import TransactionHistoryScreen from './screens/TransactionHistoryScreen'; // Ruta correcta
+import CategoriesScreen from './screens/CategoriesScreen'; // Ruta correcta
+import TransactionForm from './screens/TransactionForm'; // Ruta correcta
+import AccountsScreen from './screens/AccountsScreen'; // Asegúrate de que esta ruta sea correcta
+import PaymentMethodsScreen from './screens/PaymentMethodsScreen'; // Asegúrate de que esta ruta sea correcta
+import FrequenciesScreen from './screens/FrequenciesScreen'; // Asegúrate de que esta ruta sea correcta
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <StatusBar style="auto" />
-      <AppNavigator />
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="TransactionHistory" component={TransactionHistoryScreen} />
+                <Stack.Screen name="Categories" component={CategoriesScreen} />
+                <Stack.Screen name="TransactionForm" component={TransactionForm} />
+				<Stack.Screen name="Accounts" component={AccountsScreen} />
+				<Stack.Screen name="PaymentMethods" component={PaymentMethodsScreen} />
+				<Stack.Screen name="Frequencies" component={FrequenciesScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
 
 export default App;
--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,12 +1,10 @@
 // src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';  // Si tienes estilos globales
-import App from './App';  // Importa el componente principal App
+import { AppRegistry } from 'react-native';
+import App from './App'; // Asegúrate de que la ruta sea correcta
+import { name as appName } from './app.json';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />  // Renderiza la aplicación principal
-  </React.StrictMode>,
-  document.getElementById('root')  // Asegúrate de que esto coincida con tu index.html
-);
+AppRegistry.registerComponent(appName, () => App);
+AppRegistry.runApplication(appName, {
+  initialProps: {},
+  rootTag: document.getElementById('app-root'), // Asegúrate de que este ID coincida con tu index.html
+});
